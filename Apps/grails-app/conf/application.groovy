c2adc6c6d7aafc9cd01890cbfdab6b24b23d8aca
Bilal Ahmed <bilal@mytweetmark.com>
2017-06-02 17:19:46 -0700

    testing register flow

--- Apps/grails-app/conf/application.groovy
+++ Apps/grails-app/conf/application.groovy
@@ -14,7 +14,8 @@ grails.plugin.springsecurity.controllerAnnotations.staticRules = [
 	[pattern: '/**/js/**',       access: ['permitAll']],
 	[pattern: '/**/css/**',      access: ['permitAll']],
 	[pattern: '/**/images/**',   access: ['permitAll']],
-	[pattern: '/**/favicon.ico', access: ['permitAll']]
+	[pattern: '/**/favicon.ico', access: ['permitAll']],
+	[pattern: '/register/index', access: ['permitAll']]
 ]
 
 grails.plugin.springsecurity.filterChain.chainMap = [
