d99ea228953bae925db4aea45a04ce3eb6196b3e
Bilal Ahmed <bilal@mytweetmark.com>
2017-06-02 17:50:00 -0700

    reverting back spring security ui

--- Apps/grails-app/conf/application.groovy
+++ Apps/grails-app/conf/application.groovy
@@ -14,8 +14,7 @@ grails.plugin.springsecurity.controllerAnnotations.staticRules = [
 	[pattern: '/**/js/**',       access: ['permitAll']],
 	[pattern: '/**/css/**',      access: ['permitAll']],
 	[pattern: '/**/images/**',   access: ['permitAll']],
-	[pattern: '/**/favicon.ico', access: ['permitAll']],
-	[pattern: '/register/index', access: ['permitAll']]
+	[pattern: '/**/favicon.ico', access: ['permitAll']]
 ]
 
 grails.plugin.springsecurity.filterChain.chainMap = [
